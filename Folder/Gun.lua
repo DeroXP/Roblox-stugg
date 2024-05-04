c04c3f2ff00953e775a4cbd947dd0172d30d4f0a
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-04 16:43:24 -0500

    Update Gun.lua

--- Folder/Gun.lua
+++ Folder/Gun.lua
@@ -1,94 +1,27 @@
-local UserInputService = game:GetService("UserInputService")
-local RunService = game:GetService("RunService")
+local ReplicateStorage = game:GetService("ReplicatedStorage")
+local Auto = ReplicateStorage.HA
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RemoteEvent = ReplicatedStorage["Pre-Loaded Events"].Flash
+local UIS = game:GetService("UserInputService")
 
-local plr = game.Players.LocalPlayer
-local character
-repeat
-	character = plr.Character
-	wait(1)
-until character
+local MovementPart = Auto.MeshPart
 
-local soundId = "rbxassetid://5991592592"
-local sound = Instance.new("Sound")
-sound.SoundId = soundId
-
-local Flashlight = game.ReplicatedStorage.Flashlight:Clone()
-
-local PartWithinFlashlight = Flashlight:FindFirstChild("Handle")
-local Front = PartWithinFlashlight.MeshPart
-local Light = PartWithinFlashlight.Front.SurfaceLight
-Light.Brightness = 0
-
-local workspace = game:GetService("Workspace")
-
-local function updatePartCFrame()
-	local camera = workspace.CurrentCamera
-	if camera then
-		local offset = CFrame.new(2, -2, -4)
-		local lookVector = camera.CFrame.LookVector
-		local rotatedLookVector = (lookVector * CFrame.Angles(0, math.rad(90), 0)).Unit
-		local rotatedAngle = math.acos(rotatedLookVector:Dot(Vector3.new(0, 0, 1)))
-		local rotatedLookVectorCFrame = CFrame.new(Vector3.new(), rotatedLookVector)
-		local cFrame = camera.CFrame * CFrame.new(0, 0, -5) * CFrame.Angles(0,  math.rad(180), 0) * CFrame.new(offset.p) * rotatedLookVectorCFrame * CFrame.Angles(0, 0, rotatedAngle)
-		Front.CFrame = cFrame
+local function updateCFrame ()
+	local cam = workspace.CurrentCamera
+	if cam then
+		local offest = CFrame.new(2, -2,-4)
+		Auto.PrimaryPart.CFrame = cam.CFrame:ToWorldSpace(offest)
+		Auto.PrimaryPart.Orientation +=  Vector3.new(0, -90, 0)
 	end
 end
 
-RunService.RenderStepped:Connect(updatePartCFrame)
+game:GetService("RunService").RenderStepped:Connect(updateCFrame)
 
---[[
-local animation = Instance.new("Animation")
-animation.AnimationId = "rbxassetid://16813458158"
-]]
-
-local function isXboxButtonPressed(input)
-	return input.KeyCode == Enum.KeyCode.ButtonX or input.KeyCode == Enum.KeyCode.F
-end
-
---[[
-local function playAnimation(character)
-	local humanoid = character:FindFirstChildOfClass("Humanoid")
-	local animator = humanoid and humanoid:FindFirstChildOfClass("Animator")
-	if not animator then
-		animator = Instance.new("Animator", humanoid)
-	end
-	local animationTrack = animator:LoadAnimation(animation)
-	animationTrack:Play()
-end
-
-local function stopAnimation(character)
-	local humanoid = character:FindFirstChildOfClass("Humanoid")
-	local animator = humanoid and humanoid:FindFirstChildOfClass("Animator")
-	if not animator then return end
-	for _, track in ipairs(animator:GetPlayingAnimationTracks()) do
-		track:Stop()
-	end
-end
-]]
-
-local function toggleFlashlight(newState)
-	local state = newState or (Light.Brightness == 0)
-	if state then
-		RemoteEvent:FireServer(true)
-		Front.Parent = workspace
-	else
-		RemoteEvent:FireServer(false)
-		Front.Parent = game.ReplicatedStorage
-		--stopAnimation(plr.Character)
-	end
-	sound:Play()
-	Light.Brightness = state and 13.5 or 0
-end
-
-UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
-	if not gameProcessedEvent and isXboxButtonPressed(input) then
-		local flashlightOn = Light.Brightness > 0
-		toggleFlashlight(not flashlightOn)
-		if not flashlightOn then
-			--playAnimation(plr.Character)
+UIS.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.E then--or Enum.KeyCode.ButtonY
+		if Auto.Parent ~= workspace then
+			Auto.Parent = workspace
+		else
+			Auto.Parent = ReplicateStorage
 		end
 	end
 end)
