a4b1d67450a763b436ec9758dc4cb6b87756c807
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-04 17:05:53 -0500

    Update Gun.lua

--- Folder/Gun.lua
+++ Folder/Gun.lua
@@ -4,13 +4,20 @@ local Auto = ReplicateStorage.HA
 local UIS = game:GetService("UserInputService")
 
 local MovementPart = Auto.MeshPart
+local Left, Right = MovementPart.Left, MovementPart.Right
 
 local function updateCFrame ()
 	local cam = workspace.CurrentCamera
 	if cam then
-		local offest = CFrame.new(2, -2,-4)
-		Auto.PrimaryPart.CFrame = cam.CFrame:ToWorldSpace(offest)
-		Auto.PrimaryPart.Orientation +=  Vector3.new(0, -90, 0)
+		local moffest = CFrame.new(2, -2,-4)
+		local loffest = CFrame.new(1.5, -2.5,-4.4)
+		local roffest = CFrame.new(2.8, -2.5,-3)
+		Auto.PrimaryPart.CFrame = cam.CFrame:ToWorldSpace(moffest)
+		Left.CFrame = cam.CFrame:ToWorldSpace(loffest)
+		Right.CFrame = cam.CFrame:ToWorldSpace(roffest)
+		Left.Orientation += Vector3.new(cam.CFrame.Rotation.X + 0.337, cam.CFrame.Rotation.Y - 90 - 11.035, cam.CFrame.Rotation.Z - 14.433)
+		Right.Orientation += Vector3.new(cam.CFrame.Rotation.X + 1.55, cam.CFrame.Rotation.Y - 90 + 7.498, cam.CFrame.Rotation.Z - 11.669)
+		Auto.PrimaryPart.Orientation += Vector3.new(cam.CFrame.Rotation.X , cam.CFrame.Rotation.Y - 90, cam.CFrame.Rotation.Z)
 	end
 end
 
