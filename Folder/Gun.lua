509f721ec08e9462b738f7c4caeee15bd8c4c635
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-04 18:01:21 -0500

    Update Gun.lua

--- Folder/Gun.lua
+++ Folder/Gun.lua
@@ -6,18 +6,23 @@ local UIS = game:GetService("UserInputService")
 local MovementPart = Auto.MeshPart
 local Left, Right = MovementPart.Left, MovementPart.Right
 
+local moffest = CFrame.new(2, -2,-4)
+local loffest = CFrame.new(1.5, -2.5,-4.4)
+local roffest = CFrame.new(2.8, -2.5,-3)
+
 local function updateCFrame ()
 	local cam = workspace.CurrentCamera
 	if cam then
-		local moffest = CFrame.new(2, -2,-4)
-		local loffest = CFrame.new(1.5, -2.5,-4.4)
-		local roffest = CFrame.new(2.8, -2.5,-3)
+		local y = cam.CFrame:ToOrientation()
+		
 		Auto.PrimaryPart.CFrame = cam.CFrame:ToWorldSpace(moffest)
 		Left.CFrame = cam.CFrame:ToWorldSpace(loffest)
 		Right.CFrame = cam.CFrame:ToWorldSpace(roffest)
-		Left.Orientation += Vector3.new(cam.CFrame.Rotation.X + 0.337, cam.CFrame.Rotation.Y - 90 - 11.035, cam.CFrame.Rotation.Z - 14.433)
-		Right.Orientation += Vector3.new(cam.CFrame.Rotation.X + 1.55, cam.CFrame.Rotation.Y - 90 + 7.498, cam.CFrame.Rotation.Z - 11.669)
-		Auto.PrimaryPart.Orientation += Vector3.new(cam.CFrame.Rotation.X , cam.CFrame.Rotation.Y - 90, cam.CFrame.Rotation.Z)
+		
+		
+		Left.Orientation.Y -= Vector3.new(0, 90, 0)
+		Right.Orientation.Y -= Vector3.new(0, 90, 0)
+		Auto.PrimaryPart.Orientation.Y -= Vector3.new(0, 90, 0)
 	end
 end
 
